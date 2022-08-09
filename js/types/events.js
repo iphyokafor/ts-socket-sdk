6f0d8a90f00396a7096b31bdbe132dd2ced287a4
Okafor Ifeoma Sandra <49367561+iphyokafor@users.noreply.github.com>
2022-08-09 17:51:03 +0100

    Delete js directory

--- js/types/events.js
+++
@@ -1,7 +0,0 @@
-export var EventEnum;
-(function (EventEnum) {
-    EventEnum["message"] = "message";
-    EventEnum["log"] = "log";
-    EventEnum["connection"] = "connection";
-})(EventEnum || (EventEnum = {}));
-//# sourceMappingURL=events.js.map
